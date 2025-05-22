51f3f8739d776837f6be977069da6960373ce791
KostyaTeacher <sounddummies5@gmail.com>
2025-05-22 21:05:18 +0300

    Initial commit

---
+++ Lesson1/1.py
@@ -0,0 +1,28 @@
+import random
+b = random.randint(0, 100)
+
+answer = input("Введіть своє ім*я > ")
+
+print("I\nlove\npython")
+print(10 + 10) #Вам дано список чисел
+
+
+if b > 0:
+  print("Bilshe 0")
+else:
+  print("Menshe 0")
+print(b)
+
+for i in range(100):
+  b = random.randint(0, 100)
+  print(b)
+
+print("❤️ I love python")
+
+
+
+print(answer)
+
+
+
+
