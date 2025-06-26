09f5bddf5e9275e755d5f79d2ba4f23c922b81ef
KostyaTeacher <sounddummies5@gmail.com>
2025-06-26 19:56:15 +0300

    lessons 14

---
+++ Module2/Lessons 7/37.py
@@ -0,0 +1,7 @@
+import random
+import our_library
+#input
+print(our_library.calculate(5, 10))
+from string import printable
+
+print(printable)
---
+++ Module2/Lessons 7/our_library.py
@@ -0,0 +1,7 @@
+def calculate(a, b):
+    return a + b
+print(__name__)
+
+if __name__ == '__main__':
+    print(calculate(50, 50))
+
